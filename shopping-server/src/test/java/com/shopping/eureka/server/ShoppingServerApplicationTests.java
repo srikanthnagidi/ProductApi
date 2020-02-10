bbd5888ddb7e6aa74f72d9f9f75c779b81579fe7
Srikanth Reddy Nagidi <srikanth.nagidi@gmail.com>
2020-02-10 13:46:34 -0500

    added eureka server

---
+++ shopping-server/src/test/java/com/shopping/eureka/server/ShoppingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
